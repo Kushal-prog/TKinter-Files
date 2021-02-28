4f77d51e637c745068fec9a48bbdb303d89e85d2
Kushal-prog <77925728+Kushal-prog@users.noreply.github.com>
2021-02-28 22:32:07 +0530

    version2.0

--- DKM.py
+++ DKM.py
@@ -2,6 +2,11 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 import smtplib
+import MySQLdb
+
+conn = MySQLdb.connect(host = 'localhost', database = 'dkm_customers', user = 'root', password = 'Kushal@2006')
+cursor1 = conn.cursor()
+cursor1.execute('use dkm_customers')
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
@@ -12,30 +17,30 @@ root = tk.Tk()
 
 def Data():
 
-    try:
-        CustomerData = {
-            'Name': Name_Entry.get(),
-            'Product': Products.get(),
-            'Quantity': Quantity_Entry.get(),
-            'Email-id': Email_Entry.get()
-            }
+   
+    CustomerData = {
+        'Name': Name_Entry.get(),
+        'Product': Products.get(),
+        'Quantity': Quantity_Entry.get(),
+        'Email-id': Email_Entry.get()
+        }
+    a = str(Name_Entry.get())
+    b = str(Products.get())
+    c = str(Quantity_Entry.get())
+    d = str(Email_Entry.get())
+
+    Order = tk.messagebox.askquestion(title='Order Conformation', message=f"Do you want to confirm your order for {Quantity_Entry.get()} {Products.get()}")
+    query = f"INSERT INTO cus_data(Sn, CustomerName, ProductName, Quantity, Address, Amount, EmailAddress) values(1, '{str(a)}','{str(b)}',{str(c)}, 'adasd', 12, '{str(d)}');"
+    print(query)
+
+    if Order == 'yes':
+        confirmed = tk.messagebox.showinfo(title='Order Confirmed', message='Order Confirmed')
+        server.sendmail('kushalrakesh019@gmail.com', Email_Entry.get(),f"Dear Customer, your order for {Quantity_Entry.get()} {Products.get()} is confirmed \n Customer Name {Name_Entry.get()}")
+        # Customer = open('CustomerDetails', 'a')
+        # Customer.write(str(CustomerData) + '\n')
+        cursor1.execute(query)
+        conn.commit()
         
-
-        Order = tk.messagebox.askquestion(title='Order Conformation', message=f"Do you want to confirm your order for {Quantity_Entry.get()} {Products.get()}")
-
-
-        if Order == 'yes':
-            confirmed = tk.messagebox.showinfo(title='Order Confirmed', message='Order Confirmed')
-            server.sendmail('kushalrakesh019@gmail.com', Email_Entry.get(),f"Dear Customer, your order for {Quantity_Entry.get()} {Products.get()} is confirmed \n Customer Name {Name_Entry.get()}")
-            Customer = open('CustomerDetails', 'a')
-            Customer.write(str(CustomerData) + '\n')
-        
-
-    except smtplib.SMTPRecipientsRefused:
-        Email_error = tk.messagebox.showerror(title = 'invalid email',message= 'Invalid E-mail')
-        Email_Entry.delete(0, tk.END)
-        Data()
-
     if confirmed == 'ok':
         Name_Entry.delete(0, tk.END)
         Products.delete(0, tk.END)
@@ -72,4 +77,5 @@ Quantity_label.grid(row=1, column=2, pady = 20,sticky='w',rowspan = 2)
 Email_label.grid(row=3, column = 0)
 Submit_btn = ttk.Button(root, text = 'Submit', command = Data)
 Submit_btn.grid(row=4, column = 1)
+
 root.mainloop()
